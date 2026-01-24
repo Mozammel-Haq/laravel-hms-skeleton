80847398679ebf2267b0635d0d0c30395d239c75
Mozammel-Haq <hmojammel29@gmail.com>
2026-01-24 13:10:55 +0600

    doctorsApiPublishInitial

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\DoctorsApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('public/doctors', [DoctorsApiController::class, 'index']);
